3a67c09d7f5903a267e92953c797060e3539c164
Nadir Alkan <nadir.alkan@aurea.com>
2019-11-04 13:13:49 +0300

    First

---
+++ directory1/directory4/ALongClass.java
@@ -0,0 +1,21 @@
+package directory1.directory4;
+
+public class ALongClass {
+    public void y() {
+        int abc = 10;
+        int cde = abc * 5;
+        abc = cde + 7;
+    }
+
+    public void z (){
+        int ab = 10;
+        int cd = ab * 5;
+        ab = cd + 7;
+    }
+	
+	public void aLongMethod(){
+		int xy = 10;
+		int yz = xy * 5;
+		xy = yz + 7;
+	}
+}
